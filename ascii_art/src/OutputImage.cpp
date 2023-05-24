4a0ace7021cfd431a141305640dda6c9b8dc36d0
Bananeq <dankilom26@gmail.com>
2023-05-24 16:37:07 +0200

    updated error handling when saving ascii as image

--- ascii_art/src/OutputImage.cpp
+++ ascii_art/src/OutputImage.cpp
@@ -37,14 +37,12 @@ bool OutputImage::output(const std::vector<std::pair<std::unique_ptr<Image>, Img
             max_f_size = std::min(16000.0 / (image.first->width * image.second.scale), 16000.0 / (image.first->height * image.second.scale));
             font_size = std::max(1.0, std::min(15.0 * image.second.scale, max_f_size));
         }
+
         // Due to old ProgTest library version, I am unable to use TTF_SetFontSize
         TTF_CloseFont(font);
         font = TTF_OpenFont(FONT_PATH, font_size);
 
         SDL_RenderClear(renderer);
-        std::string image_output_name = image.second.image_path.substr(0, image.second.image_path.size() - 4) + "_ascii.png";
-        const char *out = image_output_name.c_str();
-
         SDL_Texture *texture = image.first->createTexture(renderer, font, font_size, image.second.scale);
         if (!window || !renderer || !font || texture == nullptr)
         {
@@ -60,14 +58,37 @@ bool OutputImage::output(const std::vector<std::pair<std::unique_ptr<Image>, Img
         int width, height;
         SDL_QueryTexture(texture, NULL, NULL, &width, &height);
         SDL_Surface *surface = SDL_CreateRGBSurface(0, width, height, 32, 0, 0, 0, 0);
-        SDL_RenderReadPixels(renderer, NULL, surface->format->format, surface->pixels, surface->pitch);
 
-        IMG_SavePNG(surface, out);
+        if (!surface || SDL_RenderReadPixels(renderer, NULL, surface->format->format, surface->pixels, surface->pitch) != 0)
+        {
+            TTF_CloseFont(font);
+            SDL_DestroyTexture(texture);
+            SDL_DestroyRenderer(renderer);
+            SDL_DestroyWindow(window);
+            TTF_Quit();
+            SDL_Quit();
+            return false;
+        }
+
+        std::string image_output_name = image.second.image_path.substr(0, image.second.image_path.size() - 4) + "_ascii.png";
+        const char *out = image_output_name.c_str();
+
+        if (IMG_SavePNG(surface, out) != 0)
+        {
+            TTF_CloseFont(font);
+            SDL_DestroyTexture(texture);
+            SDL_DestroyRenderer(renderer);
+            SDL_DestroyWindow(window);
+            TTF_Quit();
+            SDL_Quit();
+            return false;
+        }
         std::cout << "Image saved to " << image_output_name << std::endl;
 
         SDL_FreeSurface(surface);
         SDL_DestroyTexture(texture);
     }
+
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     TTF_CloseFont(font);
